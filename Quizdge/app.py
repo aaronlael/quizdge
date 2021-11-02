be88b6065522d29dbd899fd06cfc85fc5a72fe8c
Aaron Lael <32605887+aaronlael@users.noreply.github.com>
2021-11-02 08:43:07 -0600

    Update app.py

--- Quizdge/app.py
+++ Quizdge/app.py
@@ -1,18 +1,12 @@
-from flask import Flask, render_template
-from flask.ext.socketio import SocketIO
-import json
-
+from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-@app.route("/")
+@app.route('/')
 def index():
-  return render_template('index.html',)
-
-@socketio.on('send_message')
-def handle_source(json_data):
-  text = json_data['message'].encode('ascii', 'ignore')
-  socketio.emit('echo', {'echo': 'Server Says: '+text})
-
+    return render_template('index.html')
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
 if __name__ == "__main__":
-  socketio.run(app)
+    socketio.run(app)
