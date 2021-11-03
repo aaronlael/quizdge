fab248da040da4e08c7c4a1c9b99d032e21fe233
Aaron Lael <alael@utah.gov>
2021-11-02 19:59:43 -0600

    Changing sample

--- Quizdge/module.py
+++ Quizdge/module.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, make_response, redirect
 from flask_socketio import SocketIO, send, emit
+
 app = Flask(__name__)
 socketio = SocketIO(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @socketio.on("message")
 def handleMessage(data):
-    emit("new_message",data,broadcast=True)
+    emit("setroom",data['room'],broadcast=False)
+    emit("new_message", data['username'], broadcast=True)
+
 if __name__ == "__main__":
     socketio.run(app)
